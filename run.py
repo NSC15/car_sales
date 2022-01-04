d3bf001eb77525f63715f340ac0c90164314182c
NSC15 <nathancave123@gmail.com>
2022-01-04 11:40:05 +0000

    add new lines to text strings for better readability. add if statements to validate user function to check that the user has entered a valid integer, and a valid option of 1 or 2, currently prints back to the terminal the users option

--- run.py
+++ run.py
@@ -15,14 +15,15 @@ SHEET = GSPREAD_CLIENT.open('car_sales')
 
 print("Welcome to NC Car Sales Command Line Program. \n")
 name = input("Please enter your name : ")
-print(f"Hello there {name}")
+print("\nHello there " + name )
+
 
 def get_user_details():
     """
     function to get the users status to run different functions depending
     """
     while True:
-        user = input("Please enter '1' for Customer or '2' for Staff : ")
+        user = input("\nPlease enter '1' for Customer or '2' for Staff : ")
         
         if validate_user(user):
             print("you entered a correct user, program running")
@@ -32,11 +33,24 @@ def get_user_details():
 
 
 def validate_user(choice):
+    """
+    validate that user has entered an integer, 
+    and that the integer is either 1 or 2
+    """
     try:
         choice = int(choice)
+    
     except ValueError as e:
         print(f"Please enter an integer, you entered {e} ")
         return False
+    else:
+        if choice == 1:
+            print("\nYou chose customer")
+        elif choice == 2:
+            print("\nYou chose staff")
+        elif choice != 1 or 2:
+            print(f"Please enter a valid input, you entered {choice}")
+            return False
     
     return True
 

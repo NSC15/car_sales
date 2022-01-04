ba532a5ff469f83ade9e55110f9fc34ef2cb8b1d
NSC15 <nathancave123@gmail.com>
2022-01-04 11:09:15 +0000

    add introudction text to program, create user details function asking for an integer of 1 or 2 dependant on staff or customer, while loop to loop if user input is incorrect or to print you entered a correct user (temporary)

--- run.py
+++ run.py
@@ -12,8 +12,41 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('car_sales')
 
-sales = SHEET.worksheet('cars')
 
-data = sales.get_all_values()
+print("Welcome to NC Car Sales Command Line Program. \n")
+name = input("Please enter your name : ")
+print(f"Hello there {name}")
 
-print(data)
+def get_user_details():
+    """
+    function to get the users status to run different functions depending
+    """
+    while True:
+        user = input("Please enter '1' for Customer or '2' for Staff : ")
+        
+        if validate_user(user):
+            print("you entered a correct user, program running")
+            break
+    
+    return user
+
+
+def validate_user(choice):
+    try:
+        choice = int(choice)
+    except ValueError as e:
+        print(f"Please enter an integer, you entered {e} ")
+        return False
+    
+    return True
+
+
+
+
+
+
+
+
+
+
+get_user_details()
